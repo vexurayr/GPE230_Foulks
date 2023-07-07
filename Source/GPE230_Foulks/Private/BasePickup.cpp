04fec78bf13334c55a3c84a1ba36b482d780333d
vexurayr <70838146+vexurayr@users.noreply.github.com>
2023-07-07 00:36:04 -0700

    Milestone 4-1
    Resorted to BP for a health and speed pickup/powerup, and added a few to the level. They become invisible when overlapped and become visible (and usable) again on a timer.

---
+++ Source/GPE230_Foulks/Private/BasePickup.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePickup.h"
+
+// Sets default values
+ABasePickup::ABasePickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABasePickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABasePickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
